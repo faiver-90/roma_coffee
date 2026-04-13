5123258b8fab458880d83e8ad51ccafb60129da5
faiver90 <faiver90@gmail.com>
2026-04-13 12:20:19 +0300

    'fix long jwt'

--- config/settings.py
+++ config/settings.py
@@ -167,7 +167,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
